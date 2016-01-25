b213fbd8bcd284c4ef8909b91aa66038d95e358d
Thomas Peikert <t.s.peikert@gmail.com>
2016-01-25 15:24:58 +0100

    mount rails_admin on default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/', as: 'rails_admin'
   resources :students
 
   resources :schools
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#index'
+  # root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

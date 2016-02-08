d4cefd93f56701c624359ef4585dde55f9eaa84c
TPei <t.s.peikert@gmail.com>
2016-02-08 19:58:48 +0100

    route and translation changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
-  resources :students
-
-  resources :schools
+  mount RailsAdmin::Engine => '/schueler', as: 'rails_admin'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'application#index'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

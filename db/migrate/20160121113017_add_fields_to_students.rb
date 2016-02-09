a4e2aa1b264251d4bb059c75f694054172345c42
TPei <t.s.peikert@gmail.com>
2016-02-09 11:00:47 +0100

    removes post scaffolding migration that bricks migrations because the scaffolding was undone

--- db/migrate/20160121113017_add_fields_to_students.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldsToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :counseled, :boolean
-    add_column :students, :voluntary_internship, :boolean
-  end
-end
